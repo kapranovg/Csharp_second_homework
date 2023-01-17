0db5021d6f89dd47d388506f110f84c05219ac8b
Grigoriy Kapranov <kapranovgu@mail.ru>
2023-01-17 22:22:53 +0300

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 1: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int number = InputInt("Введите трёхзначное число ");
+
+if (ValidateNumberRank3(number))
+{
+    int secondRank = number / 10 % 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} - {secondRank}");
+}
+
+bool ValidateNumberRank3(int number)
+{
+    if (number > 99 && number < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Число не трёхзначное.");
+    return false;
+}
+
+int InputInt (string message)
+{
+    System.Console.WriteLine(message + ">");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
